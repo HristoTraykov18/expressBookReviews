42e5222c081fa992ee94035c50e34b87176f8c81
Hristo Traykov <hristotraykov98@gmail.com>
2024-07-03 10:57:15 +0300

    Moved auth function

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -9,8 +9,17 @@ const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username, password) => {
+    // Filter the users array for any user with the same username and password
+    let validusers = users.filter((user) => {
+        return (user.username === username && user.password === password);
+    });
+    // Return true if any valid user is found, otherwise false
+    if (validusers.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 //only registered users can login
