5a87d7b64004a1af1cb0b0bd677b0564f1886b3c
Hristo Traykov <hristotraykov98@gmail.com>
2024-07-03 11:14:52 +0300

    Fix for general router

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,31 +12,31 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/', function (req, res) {
-    res.send(JSON.stringify(public_users, null, 4));
+    res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
     const isbn = req.params.isbn;
-    res.send(public_users[isbn]);
+    res.send(books[isbn]);
 });
   
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
     const author = req.params.author;
-    res.send(public_users[author]);
+    res.send(books[author]);
 });
 
 // Get all books based on title
 public_users.get('/title/:title', function (req, res) {
     const title = req.params.title;
-    res.send(public_users[title]);
+    res.send(books[title]);
 });
 
 //  Get book review
 public_users.get('/review/:isbn', function (req, res) {
     const review = req.params.review;
-    res.send(public_users[review]);
+    res.send(books[review]);
 });
 
 module.exports.general = public_users;
