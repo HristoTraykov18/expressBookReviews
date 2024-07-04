89f869626633671b7e3c01aa6342889a9e6c13c9
Hristo Traykov <hristotraykov98@gmail.com>
2024-07-05 00:18:00 +0300

    Changed functions to async

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -40,18 +40,19 @@ public_users.post("/register", (req, res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/', function (req, res) {
-    res.send(JSON.stringify(books, null, 4));
+public_users.get('/', async (req, res) => {
+    const stringifiedBooks = JSON.stringify(books, null, 4);
+    res.end(stringifiedBooks);
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn', function (req, res) {
+public_users.get('/isbn/:isbn', async (req, res) => {
     const isbn = req.params.isbn;
-    res.send(books[isbn]);
+    res.end(books[isbn]);
 });
   
 // Get book details based on author
-public_users.get('/author/:author', function (req, res) {
+public_users.get('/author/:author', async (req, res) => {
     const author = req.params.author;
 
     for (let id in books) {
@@ -63,7 +64,7 @@ public_users.get('/author/:author', function (req, res) {
 });
 
 // Get all books based on title
-public_users.get('/title/:title', function (req, res) {
+public_users.get('/title/:title', async (req, res) => {
     const title = req.params.title;
 
     for (let id in books) {
